13c2a3979036c05d1018cafb419147f8bf64d271
Diana <medova1209@gmail.com>
2023-03-25 19:56:51 +0100

    add blueprint

--- app/app.py
+++ app/app.py
@@ -1,16 +1,8 @@
+from . import articles
 from flask import Flask, render_template, url_for, send_file
 
 app = Flask(__name__)
 app.config.from_object('app.config')
-
-articles_data = {
-    'moscow': {'name': 'Racism in Russia', 'reading time': '7 mins'},
-    'german': {'name': 'How to Learn German', 'reading time': '4 mins'},
-    'mtg': {'name': 'Magic the Gathering', 'reading time': '15 mins'},
-    'berlin': {'name': 'Tipps on Finding a Flat in Berlin', 'reading time': '10 mins'},
-    'empathy': {'name': 'Key to a Happy Relationship', 'reading time': '8 mins'},
-    'habit': {'name': 'Review of Some Habit Tracking Apps', 'reading time': '7 mins'},
-}
 
 degrees_data = {
     'economics': {'name': 'Bachelor of Economics', 'year of graduation': '2016'},
@@ -24,6 +16,8 @@ certificates_data = {
     'ux': {'name': 'User Experience for Web Design', 'comment': 'from LinkedIn Learning'},
 }
 
+app.register_blueprint(articles.routes.blueprint)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -31,14 +25,6 @@ def index():
 @app.route('/about-me/')
 def about_me():
     return render_template('about.html', degrees=degrees_data, certificates=certificates_data)
-
-@app.route('/articles/')
-def list_of_articles():
-    return render_template('articles.html', articles=articles_data)
-
-@app.route('/articles/<slug>')
-def each_article(slug):
-  return articles_data[slug]
 
 @app.route('/legal')
 def legal():
---
+++ app/articles/routes.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template
+
+blueprint = Blueprint('articles', __name__)
+
+articles_data = {
+    'moscow': {'name': 'Racism in Russia', 'reading time': '7 mins'},
+    'german': {'name': 'How to Learn German', 'reading time': '4 mins'},
+    'mtg': {'name': 'Magic the Gathering', 'reading time': '15 mins'},
+    'berlin': {'name': 'Tipps on Finding a Flat in Berlin', 'reading time': '10 mins'},
+    'empathy': {'name': 'Key to a Happy Relationship', 'reading time': '8 mins'},
+    'habit': {'name': 'Review of Some Habit Tracking Apps', 'reading time': '7 mins'},
+}
+
+@blueprint.route('/articles/')
+def list_of_articles():
+    return render_template('articles.html', articles=articles_data)
+
+@blueprint.route('/articles/<slug>')
+def each_article(slug):
+  return articles_data[slug]
