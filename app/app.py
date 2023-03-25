8c28e295c48a207b6045dfffcf801fe5d808a489
Diana <medova1209@gmail.com>
2023-03-25 20:47:23 +0100

    apply factory pattern

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
-from . import articles, simple_pages
 from flask import Flask, render_template, url_for, send_file
+from . import articles, simple_pages
 
-app = Flask(__name__)
-app.config.from_object('app.config')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config')
 
-app.register_blueprint(articles.routes.blueprint)
-app.register_blueprint(simple_pages.routes.blueprint)
+    register_blueprints(app)
+
+    return app
+
+# Blueprints
+def register_blueprints(app: Flask):
+    app.register_blueprint(articles.routes.blueprint)
+    app.register_blueprint(simple_pages.routes.blueprint)
