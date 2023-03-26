0c3143bcf896e9791be2bb22fe15c64a8910926d
Diana <medova1209@gmail.com>
2023-03-26 23:42:56 +0200

    install migrations

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, send_file
 from . import articles, simple_pages
-from app.extensions.database import db
+from app.extensions.database import db, migrate
 
 def create_app():
     app = Flask(__name__)
@@ -18,4 +18,8 @@ def register_blueprints(app: Flask):
 
 def register_extensions(app: Flask):
     db.init_app(app)
-# initializes the database connection of SQLAlchemy and passes the app (which is our Flask application object) as an argument
+    # initializes the database connection of SQLAlchemy and passes the app (which is our Flask application object) as an argument
+    migrate.init_app(app, db, compare_type=True)
+    #the first parameter app tells where to look for the models that are imported in our app
+    #the second parameter db tells the script where to find the database that should be dated with the new database tables
+    #compare_type is a configuration option; it will enable any changes we make to column types in our models and allow us to create migrations for it
