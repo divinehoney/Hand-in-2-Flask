3981ae38791ac251eabae0e126e9611cf5463aff
Diana <medova1209@gmail.com>
2023-03-26 23:20:28 +0200

    define a model with SQLAlchemy

--- app/articles/models.py
+++ app/articles/models.py
@@ -1,6 +1,8 @@
-class Article():
-    id = ''
-    slug = ''
-    name = ''
-    date_published = ''
-    reading_time = ''
+from app.extensions.database import db
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(80))
+    date_published = db.Column(db.Date)
+    reading_time = db.Column(db.String(15))
