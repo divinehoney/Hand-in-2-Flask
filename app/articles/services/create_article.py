ae94a42f4c7916dc1b8274415c5cc548dbdfaf51
Diana <medova1209@gmail.com>
2023-04-27 11:36:38 +0200

    remove 'content' from 'create_article'

--- app/articles/services/create_article.py
+++ app/articles/services/create_article.py
@@ -3,7 +3,6 @@ from app.articles.models import Article
 def create_article(form_data):
     article_to_add = Article(
         title = form_data.get('title'),
-        text = form_data.get('content'),
         reading_time = form_data.get('reading_time')
     )
     article_to_add.save()
